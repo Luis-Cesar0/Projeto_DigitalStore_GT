047601a065cd2f34e440b5260aaef35dddb62f7b
Luis César <luiscesarn1@gmail.com>
2024-07-16 10:12:42 -0300

    cirando a navBar do Digital Store

--- Digital-Store/src/App.jsx
+++ Digital-Store/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Cabesalho from './components/Header/Cabesalho'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Cabesalho/>
      
-     <h1>Oi</h1>
     </>
   )
 }
---
+++ Digital-Store/src/components/Header/Cabesalho.jsx
@@ -0,0 +1,33 @@
+import './Cabesalho.css'
+
+
+export default function Cabesalho() {
+    return(<header>
+    <nav>
+      <div id="top-nav" className="nav-filhos d-flex justify-content-between align-items-center">
+        <a href="#" >
+            <img src="../../images/logo_DStore.png" alt="logo_DStore"/>
+            </a>
+        <form action="#" method="get" id="form-search-nav" >
+          <input type="search" name="pesProduto" id="pesProduto"  placeholder="Pesquisar produto..."/>
+          <button type="submit" className="nav-btn " id="btnPesquisa"><i className="bi bi-search"></i></button>
+        </form>
+       <div id="cadEntraCarinho">
+          <a href="#" className="nav-btn " id="link_cadastro">Cadastre-se</a>
+          <a href="#" className="nav-btn btn_primary" id="btn_entra">Entra</a>
+          <a href="#" id="btn-carrinho ">
+            <img src="img/carrinhoCompras.png" alt="carinho De compras"/>
+          </a>
+       </div>
+      </div>
+      <div id="bot-nav" className="nav-filhos ">
+        <ul id="listNavLinks">
+          <li className="listLinks listLinksConfime"><a href="#" >Home</a></li>
+          <li className="listLinks"><a href="#">Produtos</a></li>
+          <li className="listLinks"><a href="#">Categoria</a></li>
+          <li className="listLinks"><a href="#">Meus Pedidos</a></li>
+        </ul>
+      </div>
+    </nav>
+</header>)
+}
