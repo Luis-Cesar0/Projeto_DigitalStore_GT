a96b8de400a8babbda7c6692d0269c043549a2bd
LUIS CESAR EUGENIO AMORIM <luiscesarn1@gmail.com>
2024-07-17 14:52:54 -0300

    finalizando estilo do card produto

--- Digital-Store/src/App.jsx
+++ Digital-Store/src/App.jsx
@@ -3,34 +3,36 @@ import './App.css'
 import Header from './components/Header/Header'
 // import Section from './components/Section/Section'
 import CardProduto  from './components/ProductCard/CardProduto'
-// import Footer from './components/Footer/Footer'
-import ProductCard  from './components/ProductCard/ProductCard'
+import Footer from './components/Footer/Footer'
+// import ProductCard  from './components/ProductCard/ProductCard'
 import sapatoProduto from"./images/sapato_azul_Dstore.png"
 
 
 
 function App() {
   
-let produtos= [
-  {
-    name: "Nome do produto 1",
-    image: sapatoProduto,
-    price: 200,
-    priceDiscount: 149.9
-  },
-  {
-    name: "Nome do produto 2",
-    image: "./images/sapato_azul_Dstore.png",
-    price: 49.9
-  }
-  ]
+// let produtos= [
+//   {
+//     name: "Nome do produto 1",
+//     image: sapatoProduto,
+//     price: 200,
+//     priceDiscount: 149.9,
+//     categoria: 'Tênis'
+//   },
+//   {
+//     name: "Nome do produto 2",
+//     image: "./images/sapato_azul_Dstore.png",
+//     price: 49.9,
+//     categoria: 'Blusa'
+//   }
+//   ]
 
   return (
     <div>
       <Header/>
       {/* <ProductCard/> */}
-      <CardProduto img={produtos[0].image} nome={produtos[0].name} preco={produtos[0].price} precoDesconto={produtos[0].priceDiscount}/>
-      {/* <Footer/> */}
+      {/* <CardProduto img={produtos[0].image} nome={produtos[0].name} preco={produtos[0].price} precoDesconto={produtos[0].priceDiscount} categoria={produtos[0].categoria}/> */}
+      <Footer/>
 
     </div>
   ) 
