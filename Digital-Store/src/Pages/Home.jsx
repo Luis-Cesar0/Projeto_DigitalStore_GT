13c34773d53907736318926d457b763ed44a7f3e
LUCAS DA SILVA DUARTE <LUCAS6224@DIGITALCOLLEGE.LOCAL>
2024-07-18 16:40:48 -0300

    adição do componente oferta especial

--- Digital-Store/src/Pages/Home.jsx
+++ Digital-Store/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import Gallery from "../components/Gallery/Gallery"
-
+import OfertaEspecial from "../components/OfertaEspecial/OfertaEspecial"
 
 export default function Home(){
     return(
         <main>
             <Gallery/>
-            
+            <OfertaEspecial/>
         </main>
     )
 }
