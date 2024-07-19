a8c7cfd8729552b70dfb0c1b3b7c201851fd0eeb
Luis César <luiscesarn1@gmail.com>
2024-07-19 00:28:14 -0300

    criando galeria dinamica

--- Digital-Store/src/components/Gallery/Gallery.jsx
+++ Digital-Store/src/components/Gallery/Gallery.jsx
@@ -1,78 +1,55 @@
 import './Gallery.css'
-import sapatoBaner from '../../images/Sapato_Ornamento.png'
 import fogo from '../../images/fogo.png'
 
 
-function Gallery({className,width,height,radius,showThumbs,images }) {
+function Gallery({ClassName,Width,height,radius,showThumbs,images }) {
   
   return (
-    <div id="galeria" class="carousel slide carousel-dark">
-
-        <div class="carousel-indicators" id="cont-indicators">
-          <button type="button" data-bs-target="#galeria" data-bs-slide-to="0" class="indicador active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#galeria" data-bs-slide-to="1" class="indicador" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#galeria" data-bs-slide-to="2" class="indicador" aria-label="Slide 3"></button>
+    <div id="galeria" className={`carousel slide carousel-dark ${ClassName}`} style={
+      {width: Width, height: height, borderRadius: radius}
+    }>
+        {/* container dos botões da parte debaixo do slide */}
+        <div className="carousel-indicators" id="cont-indicators">
+          {/* Usando o map com o array de imagens para cria os botôes para cada slid */}
+          {images.map((img, index) => {
+            return(
+              <button type="button" data-bs-target="#galeria" data-bs-slide-to={index} className={index == 0 ? "indicador active" :"indicador"} aria-current="true" aria-label={`Slide ${index + 1}`} key={index}>{showThumbs == true && img}</button>
+            )
+          })}
         </div>
 
-        <div class="carousel-inner" id="container-galeria">
+          {/* container onde fica todos os slides */}
+        <div className="carousel-inner" id="container-galeria">
+            {/* Usando o map com o array de imagens para cria os As imagens de cada slide */}
+            {images.map((img,index)=>{
+              return(
+                <div className={index == 0 ? "carousel-item active" : "carousel-item" } key={index}>
+                  <div className="conteudo-galeria">
+                        {/* showThumbs para esconder o texto promocional */}
+                      {showThumbs != true &&
+                        <div className="texto-galeria">
+                        <p className="alerta-promo">Melhores ofertas personalizadas</p>
+                        <h2>Queima de stoque Nike <img src={fogo} alt="fogo" /></h2>
+                        <p className="detalhe-galeria">Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
+                        <button className="btn-primary">Ver Oferta</button>
+                      </div>}
 
-          <div class="carousel-item active ">
-            <div class="conteudo-galeria">
-  
-              <div class="texto-galeria">
-                <p class="alerta-promo">Melhores ofertas personalizadas</p>
-                <h2>Queima de stoque Nike 🔥</h2>
-                <p class="detalhe-galeria">Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
-                <button class="btn-primary">Ver Oferta</button>
-              </div>
-
-              <img src={sapatoBaner} class="img-galeria" alt="..."/>
-            </div>
-
-          </div>
-
-          <div class="carousel-item">
-
-            <div class="conteudo-galeria">
-
-              <div class="texto-galeria">
-                <p class="alerta-promo">Melhores ofertas personalizadas</p>
-                <h2>Queima de stoque Nike 🔥</h2>
-                <p class="detalhe-galeria">Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
-                <button class="btn-primary">Ver Oferta</button>
-              </div>
-
-            
-
-             <img src={sapatoBaner} class="img-galeria" alt="..."/>
-          </div>
-          </div>
-
-          <div class="carousel-item">
-
-            <div class="conteudo-galeria">
-              <div class="texto-galeria">
-                <p class="alerta-promo">Melhores ofertas personalizadas</p>
-                <h2>Queima de stoque Nike 🔥</h2>
-                <p class="detalhe-galeria">Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
-                <button class="btn-primary">Ver Oferta</button>
-              </div>
-           
-
-              <img src={sapatoBaner} class="img-galeria" alt="..."/>
-            </div>
-          </div>
-
+                    <img src={img} className='img-slide' alt='Imagem do Produto'/>
+                  </div>
+                </div>
+              )
+            })}
         </div>
 
+        {/* setas que movem o slides */}
         <div id="arrow-galeria">
-          <button class="carousel-control-prev " type="button" data-bs-target="#galeria" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+          <button className="carousel-control-prev " type="button" data-bs-target="#galeria" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#galeria" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+          <button className="carousel-control-next" type="button" data-bs-target="#galeria" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
           </button>
         </div>
 
