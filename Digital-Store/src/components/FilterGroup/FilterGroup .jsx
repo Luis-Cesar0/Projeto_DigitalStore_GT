d964e6567479ea8da80bb4913f02e34ced4c4382
Luis César <luiscesarn1@gmail.com>
2024-07-20 20:41:05 -0300

    aintigo filtrar,agora com nova estrutura

---
+++ Digital-Store/src/components/FilterGroup/FilterGroup .jsx
@@ -0,0 +1,41 @@
+
+import './FilterGroup .css';
+
+export default function FilterGroup ({listaOpcos,Radio,titulo}) {
+    return (
+               <>
+                
+                   
+                {Radio != true ? 
+                    <div className='cont-Categorias' >
+                        <h5 className="subtitulos">{titulo}</h5>
+                    {
+                        listaOpcos.map((opcao, index) => {
+                            return (
+                                <div key={index}>
+                                    <input type="checkbox" value={opcao.value} name={opcao.value} />
+                                    <label htmlFor={opcao.value}>{opcao.text}</label>
+                                </div>
+                            )
+                    })
+                        }
+                    </div>
+                :
+                    <div className='cont-Categorias'>
+                        <h5 className="subtitulos">{titulo}</h5>
+                        {
+                            
+                            listaOpcos.map((opcao, index) => {
+                                return(
+                                <div key={index}>
+                                    <input type="radio" value={opcao.value} name={titulo} id={opcao.text} style={{ border: "1px solid black" }} />
+                                    <label htmlFor={opcao.text}>{opcao.text}</label>
+                                </div>)
+                            })
+                        }
+
+                    </div>}
+                   
+               </>
+    );
+}
