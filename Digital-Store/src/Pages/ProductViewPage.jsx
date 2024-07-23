b09ef7dde7289029d245ff729c47f8f51b9f62c1
LUIS CESAR EUGENIO AMORIM <luiscesarn1@gmail.com>
2024-07-23 15:01:32 -0300

    criando pagina de visualização  de produto

--- Digital-Store/src/Pages/ProductViewPage.jsx
+++ Digital-Store/src/Pages/ProductViewPage.jsx
@@ -1,5 +1,6 @@
 import { contextoImgProduto } from "../Context/ContextImgProdut"
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 
 import React from 'react';
@@ -8,25 +9,48 @@ import React from 'react';
 // import SlideProduto from '../components/SlideProduto/SlideProduto';
 import Gallery from '../components/Gallery/Gallery'
 import BuyBox from "../components/BuyBox/BuyBox";
+import ProductiListing from "../components/ProductListing/ProductListing";
+import Section from "../components/Section/Section";
 
 import './ProductViewPage.css'
 
 export default function ProductViewPage(){
     const { imgProduto, setImgProduto } = useContext(contextoImgProduto);
     let imagemBaner = imgProduto.imgDetalhesProdutos
+
+    let listadestaque =[]
+    for (let i = 0; i < 4; i++) {
+        listadestaque[i]= imgProduto.produtos[i]
+        
+    }
     return (
-        <div className="princial-detalhes">
-            {/* <DetalhesProduto /> */} 
-            <Gallery images={imagemBaner} showThumbs ClassName="container-preview" radius={5} height={600}/>
-            <BuyBox stars={4.7} 
-            categoria={'Casual'} 
-            Marca={'Nike'} 
-            description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'} 
-            name={'Tênis Nike Revolution 6 Next Nature Masculino'} 
-            prece={219} 
-            priceDiscount={119} 
-            rating={90} 
-            reference={'38416711'}/>
+        <div className="principal-detlhes" >
+              <div className="caminho">
+                <Link to={'/'} className="Homelink caminhoLink">Home</Link>
+                <span>/</span>
+                <Link to={'/produtos'} className="ProdutosLink caminhoLink"> Produtos</Link>
+                <span>/</span>
+                <Link to={'/produtos'} className="ModeloLink caminhoLink"> Tênis</Link>
+                <span>/</span>
+                <Link to={'/produtos'} className="MarcaLink caminhoLink"> Nike</Link>
+                <span>/</span>
+                <Link to={'/produtos'} className="NomeLink caminhoLink "> Tênis Nike Revolution 6 Next Nature Masculino</Link>
+              </div>
+            <div className="detalhes">
+                <Gallery images={imagemBaner} showThumbs ClassName="container-preview" radius={5} height={600}/>
+                <BuyBox stars={4.7} 
+                categoria={'Casual'} 
+                Marca={'Nike'} 
+                description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'} 
+                name={'Tênis Nike Revolution 6 Next Nature Masculino'} 
+                prece={219} 
+                priceDiscount={119} 
+                rating={90} 
+                reference={'38416711'}/>
+            </div>
+            <Section title={'Produtos em alta'} link="2" Width={1800}>
+                    <ProductiListing listProduto={listadestaque} url={'produtos/:id'}/>
+            </Section>
         </div>
 
  
--- Digital-Store/src/components/BuyBox/BuyBox.jsx
+++ Digital-Store/src/components/BuyBox/BuyBox.jsx
@@ -9,7 +9,7 @@ export default function BuyBox({name,reference,Marca,categoria,stars,rating,prec
     const handleClickColor= (color) => {
         setActiveColor(color);
     };
-    const [activeBtn, setActiveBtn] = useState('azul');
+    const [activeBtn, setActiveBtn] = useState('39');
 
     const handleClickBtn = (numero) => {
         setActiveBtn(numero);
@@ -58,18 +58,18 @@ export default function BuyBox({name,reference,Marca,categoria,stars,rating,prec
                     </div>
                     <div className="tamanho">
                         <h5>Tamanho</h5>
-                        <button className={activeBtn === '39' ? 'acticolor' : ''} onClick={() => handleClickBtn('39')}>39</button>
-                        <button className={activeBtn === '40' ? 'acticolor' : ''} onClick={() => handleClickBtn('40')}>40</button>
-                        <button className={activeBtn === '41' ? 'acticolor' : ''} onClick={() => handleClickBtn('41')}>41</button>
-                        <button className={activeBtn === '42' ? 'acticolor' : ''}onClick={() => handleClickBtn('42')}>42</button>
-                        <button className={activeBtn === '43' ? 'acticolor' : ''} onClick={() => handleClickBtn('43')}>43</button>
+                        <button className={activeBtn === '39' ? 'actiBtn' : ''} onClick={() => handleClickBtn('39')}>39</button>
+                        <button className={activeBtn === '40' ? 'actiBtn' : ''} onClick={() => handleClickBtn('40')}>40</button>
+                        <button className={activeBtn === '41' ? 'actiBtn' : ''} onClick={() => handleClickBtn('41')}>41</button>
+                        <button className={activeBtn === '42' ? 'actiBtn' : ''}onClick={() => handleClickBtn('42')}>42</button>
+                        <button className={activeBtn === '43' ? 'actiBtn' : ''} onClick={() => handleClickBtn('43')}>43</button>
                     </div>
                     <div className='cores'>
                         <h5>Tamanho</h5>
-                        <button className={activeColor === 'azul' ? 'acticolor' : ''} onClick={() => handleClickColor('azul')}><p className='azul'></p></button>
-                <button className={activeColor === 'vermelho' ? 'acticolor' : ''} onClick={() => handleClickColor('vermelho')}><p className='vermelho'></p></button>
-                <button className={activeColor === 'cinza' ? 'acticolor' : ''} onClick={() => handleClickColor('cinza')}><p className='cinza'></p></button>
-                <button className={activeColor === 'roxo' ? 'acticolor' : ''} onClick={() => handleClickColor('roxo')}><p className='roxo'></p></button>
+                        <button className={activeColor === 'azul' ? 'actiColor' : ''} onClick={() => handleClickColor('azul')}><p className='azul'></p></button>
+                <button className={activeColor === 'vermelho' ? 'actiColor' : ''} onClick={() => handleClickColor('vermelho')}><p className='vermelho'></p></button>
+                <button className={activeColor === 'cinza' ? 'actiColor' : ''} onClick={() => handleClickColor('cinza')}><p className='cinza'></p></button>
+                <button className={activeColor === 'roxo' ? 'actiColor' : ''} onClick={() => handleClickColor('roxo')}><p className='roxo'></p></button>
                     </div>
                     <a href="#" className='btncomprar'>COMPRAR</a>
             
