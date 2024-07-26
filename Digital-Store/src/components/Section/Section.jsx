380d2aee57279981f81d8ec8c40ad82915640c5a
LUIS CESAR EUGENIO AMORIM <luiscesarn1@gmail.com>
2024-07-26 16:28:55 -0300

    trocando link do select

--- Digital-Store/src/components/Section/Section.jsx
+++ Digital-Store/src/components/Section/Section.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Section.css';
+import { Link } from 'react-router-dom';
 
 export default function Section({ title, titleAlign = 'left', children, link = '#',Width}) {
     return (
@@ -8,7 +9,7 @@ export default function Section({ title, titleAlign = 'left', children, link = '
                 {/* define onde e como o titulo vai ser exibido e o link */}
                 {title && titleAlign === 'left' && <h2 id='section-title'>{title}</h2>}
                 {title && titleAlign === 'center' && <h2 className="center" id='section-title'>{title}</h2>}
-                {link !== '#' && <a href={link} className="section-link">Ver Todos <i className="bi bi-arrow-right"></i></a>}
+                {link !== '#' && <Link to={link} className="section-link">Ver Todos <i className="bi bi-arrow-right"></i></Link>}
             </div>
             <div className="section-interna">
                 {/* elemento a ser adicionado dentro da sction */}
